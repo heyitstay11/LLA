26644e67dfd736b321935eecc0a7f740ae0fc60c
tahir <chaudharitahir1@gmail.com>
2022-09-02 20:49:49 +0530

    added logo

--- lla_client/src/components/Footer/index.jsx
+++ lla_client/src/components/Footer/index.jsx
@@ -1,21 +1,19 @@
+import { useThemeContext } from "../../context/theme";
+
 const Footer = () => {
+  const { theme } = useThemeContext();
   return (
     <footer className="text-gray-600 body-font dark:bg-slate-900 dark:text-white">
       <div className="h-0.5 w-100 bg-yellow-500"></div>
       <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
         <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900 dark:text-white">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            className="w-10 h-10 text-white p-2 bg-yellow-500 rounded-full"
-            viewBox="0 0 24 24"
-          >
-            <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-          </svg>
+          <img
+            src="./lm.png"
+            alt=""
+            className={`w-20 h-15 text-white p-2  rounded-full ${
+              theme !== "dark" ? "darklogo" : ""
+            }`}
+          />
           <span className="ml-3 text-xl">Lingomax</span>
         </a>
         <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4 dark:text-white">
