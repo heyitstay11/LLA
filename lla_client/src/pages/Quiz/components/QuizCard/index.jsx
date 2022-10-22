3ca26294128531d4a8eb01b6dff7d69e03ce1e7b
heyitstay11 <heyitstay011@gmail.com>
2022-10-22 16:49:56 +0530

    audio-1

--- lla_client/src/pages/Quiz/components/QuizCard/index.jsx
+++ lla_client/src/pages/Quiz/components/QuizCard/index.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState, useRef } from "react";
 import { NavLink } from "react-router-dom";
 import { quizdata } from "./data";
 
@@ -47,32 +47,60 @@ const QuizImage = ({ options, question, imgsrc, next }) => {
   );
 };
 
-const QuizAudio = ({ question, desc, options, next }) => {
-  const Audcomp = ({ data }) => {
-    return (
-      <div className="p-2 sm:w-1/2 w-full">
-        <div className="bg-gray-100 rounded flex p-4 h-full items-center justify-between dark:bg-slate-500 dark:text-white">
-          <img src="./speaker-svgrepo-com.svg" className="w-6 h-6" />
-          <span className="title-font font-medium">{data}</span>
-          <svg
-            fill="none"
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="3"
-            className="text-yellow-500 w-6 h-6 flex-shrink-0 mr-4"
-            viewBox="0 0 24 24"
-          >
-            <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
-            <path d="M22 4L12 14.01l-3-3"></path>
-          </svg>
-        </div>
+const audios = [
+  "/audio/a1.wav",
+  "/audio/a2.wav",
+  "/audio/a3.wav",
+  "/audio/a4.wav",
+];
+
+const Audcomp = ({ data, handleChange, index }) => {
+  return (
+    <div className="p-2 sm:w-1/2 w-full">
+      <div className="bg-gray-100 rounded flex p-4 h-full items-center justify-between dark:bg-slate-500 dark:text-white">
+        <img
+          onClick={() => handleChange(audios[index])}
+          src="./speaker-svgrepo-com.svg"
+          className="w-6 h-6"
+        />
+        <span className="title-font font-medium">{data}</span>
+        <svg
+          fill="none"
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="3"
+          className="text-yellow-500 w-6 h-6 flex-shrink-0 mr-4"
+          viewBox="0 0 24 24"
+        >
+          <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
+          <path d="M22 4L12 14.01l-3-3"></path>
+        </svg>
       </div>
-    );
+    </div>
+  );
+};
+
+const QuizAudio = ({ question, desc, options, next }) => {
+  /**
+   * @type {React.MutableRefObject<HTMLAudioElement>}
+   */
+  const audioRef = useRef(null);
+  const [currentAudio, setCurrentAudio] = useState("/audio/a1.wav");
+
+  const handleChange = (src) => {
+    setCurrentAudio(() => src);
+    audioRef.current.currentTime = 0;
+    audioRef.current.pause();
+    audioRef.current.load();
+    audioRef.current.play();
   };
   return (
     <section className="text-gray-600 body-font dark:bg-slate-900 dark:text-white">
       <div className="container px-5 py-24 mx-auto dark:bg-slate-900 dark:text-white">
+        <audio ref={audioRef} loop={false} controls={false}>
+          <source src={currentAudio} />
+        </audio>
         <div className="text-center mb-20">
           <h1 className="sm:text-3xl text-2xl font-medium text-center title-font text-gray-900 mb-4 dark:bg-slate-900 dark:text-white">
             {question}
@@ -83,7 +111,9 @@ const QuizAudio = ({ question, desc, options, next }) => {
         </div>
         <div className="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2 ">
           {options.map((e, i) => {
-            return <Audcomp key={i} data={e} />;
+            return (
+              <Audcomp key={i} data={e} handleChange={handleChange} index={i} />
+            );
           })}
         </div>
         <button
