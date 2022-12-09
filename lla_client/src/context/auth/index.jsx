57bc68c355515c451caba49c4d1bacd8ceb58512
heyitstay11 <heyitstay011@gmail.com>
2022-12-09 21:25:18 +0530

    non-volatile-auth

--- lla_client/src/context/auth/index.jsx
+++ lla_client/src/context/auth/index.jsx
@@ -1,10 +1,15 @@
 import { useMemo, useContext, useState, createContext } from "react";
 
-const userData = window.sessionStorage.getItem("lla_user");
+const userData = window.localStorage.getItem("lla_user");
 const AuthContext = createContext({});
 
 export const AuthProvider = ({ children }) => {
-  const [auth, setAuth] = useState(JSON.parse(userData) || {});
+  const [auth, setAuthn] = useState(JSON.parse(userData) || {});
+
+  const setAuth = (data) => {
+    setAuthn(data);
+    window.localStorage.setItem("lla_user", JSON.stringify(data));
+  };
 
   const value = useMemo(() => {
     return { auth, setAuth };
